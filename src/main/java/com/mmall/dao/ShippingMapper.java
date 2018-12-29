bdf59bfed7932bc8dbb8228ff781670e0c1c7af2
jarry <15958002454@163.com>
2018-12-29 22:04:46 +0800

    the model of shipping is ok,and the test is finish

--- src/main/java/com/mmall/dao/ShippingMapper.java
+++ src/main/java/com/mmall/dao/ShippingMapper.java
@@ -1,6 +1,9 @@
 package com.mmall.dao;
 
 import com.mmall.pojo.Shipping;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ShippingMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +17,16 @@ public interface ShippingMapper {
     int updateByPrimaryKeySelective(Shipping record);
 
     int updateByPrimaryKey(Shipping record);
+
+    //custom    防止横向越权问题
+    int deleteByShippingIdUserId(@Param("shippingId")Integer shippingId,@Param("userId")Integer userId);
+
+    //custom
+    int updateByShipping(Shipping shipping);
+
+    //custom
+    Shipping selectByShippingIdUserId(@Param("shippingId")Integer shippingId,@Param("userId")Integer userId);
+
+    //custom
+    List<Shipping> listByUserId(Integer userId);
 }
